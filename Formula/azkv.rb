00e5ecf19c022da02b99f9b34866b15155c1435a
Alexey Ryazhskikh <musukvl@gmail.com>
2026-02-13 21:49:43 +0200

    Release v1.0.1 - Fix build without PublishTrimmed

--- Formula/azkv.rb
+++ Formula/azkv.rb
@@ -1,17 +1,17 @@
 class Azkv < Formula
   desc "Terminal UI for managing Azure Key Vaults"
   homepage "https://github.com/musukvl/azkv"
-  version "1.0.0"
+  version "1.0.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/musukvl/azkv/releases/download/v1.0.0/azkv-1.0.0-osx-arm64.tar.gz"
-      sha256 "5c1245bfbc2acfed6eed556d7ae5b91e7c4fab125c1c1c874f0ed075ea01dafc"
+      url "https://github.com/musukvl/azkv/releases/download/v1.0.1/azkv-1.0.1-osx-arm64.tar.gz"
+      sha256 "a04cd7bb72aaace81e63894f000cc8d547288d7de78ea64d741fd259bba5e1db"
     end
     on_intel do
-      url "https://github.com/musukvl/azkv/releases/download/v1.0.0/azkv-1.0.0-osx-x64.tar.gz"
-      sha256 "e07e397ca16b760d48a52c46b758e2b3b1bbd68c58fa770e21c6642fc13cb2a7"
+      url "https://github.com/musukvl/azkv/releases/download/v1.0.1/azkv-1.0.1-osx-x64.tar.gz"
+      sha256 "1111366749cdd8351bfd691ea8946be01ade9b1e39cde39df1e75b1d7f0d5504"
     end
   end
 
